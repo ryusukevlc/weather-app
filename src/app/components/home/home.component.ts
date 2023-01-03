ea708f49bce6d3172a0cad09c7d2115bb8b6585e
ARATA\ryusu <ryusuke14520@gmail.com>
2023-01-03 18:22:10 +0900

    commit

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  // 気温
+  public temperature: string;
+  // 湿度
+  public humidity: string;
+  // 風速
+  public wind: string;
 
+  /**
+   * 天気情報の取得
+   */
+  public getWeatherInfo() {}
 }
